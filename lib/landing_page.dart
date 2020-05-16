b56e8be0d14c33f4512a952b6d15519ffadae714
amansri2502 <33771267+amansri2502@users.noreply.github.com>
2020-05-16 07:31:28 +0530

    added touch functionality

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LandingPage extends StatelessWidget {
   @override
@@ -32,6 +33,62 @@ class LandingPage extends StatelessWidget {
             indent: 70,
             endIndent: 70,
             height: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(onTap: () async{
+              const url = 'tel:+91 9999999999';
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+
+            },
+                          child: Card(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+91-9999999999",
+                    style: TextStyle(
+                        color: Colors.teal, fontFamily: "SansPro", fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.teal,
+                ),
+                title: GestureDetector(
+                  onTap: () async {
+                    const url = 'mailto:aman.sri25@gmail.com';
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                  child: Text(
+                    "aman.sri25@gmail.com",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: "SansPro",
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
           )
         ],
       ),
